f1b3951450be1f7f51c611df93696eac76241cc4
tahlilislam <tahlilislam@gmail.com>
2024-01-02 13:33:57 -0800

    completed exercise

--- makeText.js
+++ makeText.js
@@ -1,2 +1,44 @@
 /** Command-line tool to generate Markov text. */
+const fs = require('fs');
+const markov = require("./markov");
+const axios = require("axios");
 
+function generateText(text) {
+    let mm = new markov.MarkovMachine(text);
+    console.log(mm.makeText());
+}
+
+// Read file and generate text from it //
+function makeText(path) {
+    fs.readFile(path, "utf8", (err, data) => {
+        if(err) {
+            console.error(`Cannot read file: ${path}: ${err}`)
+            process.exit(1);
+        } else {
+            generateText(data);
+        }
+    })
+}
+/** read URL and make text from it. */
+async function makeURLText(url) {
+    let resp;
+    try {
+        resp = await axios.get(url);
+    } catch (err) {
+        console.error(`Cannot read URL: ${url}: ${err}`)
+    }
+    console.log(resp.data);
+    generateText(resp.data);
+}
+console.log(process.argv)
+
+let [method, path] = process.argv.slice(2);
+if (method === "file") {
+    console.log(`...generated text from ${method} ${path}...`)
+    text = makeText(path)
+} else if (method === 'url') {
+    console.log(`...generated text from that ${method}...`)
+    text = makeURLText(path)
+} else {
+    console.error(`ERROR!!! Unknown method ${method}. Use "file" or "url".`)
+}
--- markov.js
+++ markov.js
@@ -1,13 +1,11 @@
 /** Textual markov chain generator */
 
-
 class MarkovMachine {
-
   /** build markov machine; read in text.*/
 
   constructor(text) {
     let words = text.split(/[ \r\n]+/);
-    this.words = words.filter(c => c !== "");
+    this.words = words.filter((c) => c !== "");
     this.makeChains();
   }
 
@@ -18,12 +16,39 @@ class MarkovMachine {
 
   makeChains() {
     // TODO
+    this.chains = {};
+    for (let i = 0; i < this.words.length; i++) {
+      let currWord = this.words[i];
+      let nextWord = this.words[i + 1] || null;
+      // does the chains obj have the current word as key? if so push next word values into existing array,
+      // else create a new arry with the next word as the current word
+      this.chains[currWord]
+        ? this.chains[currWord].push(nextWord)
+        : (this.chains[currWord] = [nextWord]);
+    }
   }
 
+  /** Pick random choice from array */
+  static choice(ar) {
+    // picking arr element at random index
+    return ar[Math.floor(Math.random() * ar.length)];
+  }
 
   /** return random text from chains */
 
   makeText(numWords = 100) {
     // TODO
+    let word = MarkovMachine.choice(Object.keys(this.chains));
+    let wordsArr = [];
+
+    while (wordsArr.length < numWords && word !== null) {
+      wordsArr.push(word);
+      // Choose the next word randomly from the array associated with the current word
+      let nextWord = this.chains[word];
+      word = MarkovMachine.choice(nextWord);
+    }
+    return wordsArr.join(" ");
   }
 }
+
+module.exports = {MarkovMachine};
